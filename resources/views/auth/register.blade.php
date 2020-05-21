e701d3c03c6896854b7ed5e7633045abbdb56cd0
masal9pse <returnymgstokh@icloud.com>
2020-05-22 03:30:55 +0900

    登録画面のバリデーションの詳細化

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,9 +23,9 @@
    <div class="form-group @error('name')has-error @enderror">
     <label>名前</label>
     <input type="text" name="name" class="form-control" placeholder="名前を入力してください">
-    @if($errors->any())
+    @if($errors->has('name'))
     <div class="alert alert-danger">
-     @foreach($errors->all() as $message)
+     @foreach($errors->get('name') as $message)
      <p>{{ $message }}</p>
      @endforeach
     </div>
@@ -34,9 +34,9 @@
    <div class="form-group @error('email')has-error @enderror">
     <label>メールアドレス</label>
     <input type="email" name="email" class="form-control" placeholder="メールアドレスを入力してください">
-    @if($errors->any())
+    @if($errors->has('email'))
     <div class="alert alert-danger">
-     @foreach($errors->all() as $message)
+     @foreach($errors->get('email') as $message)
      <p>{{ $message }}</p>
      @endforeach
     </div>
@@ -47,9 +47,9 @@
     <label>パスワード</label>
     <em>6文字以上入力してください</em>
     <input type="password" name="password" class="form-control" placeholder="パスワードを入力してください">
-    @if($errors->any())
+    @if($errors->has('password'))
     <div class="alert alert-danger">
-     @foreach($errors->all() as $message)
+     @foreach($errors->get('password') as $message)
      <p>{{ $message }}</p>
      @endforeach
     </div>
@@ -74,9 +74,9 @@
    <div class="form-group @error('self_introduction')has-error @enderror">
     <label>自己紹介文</label>
     <textarea class="form-control" name="self_introduction" rows="10"></textarea>
-    @if($errors->any())
+    @if($errors->has('self_introduction'))
     <div class="alert alert-danger">
-     @foreach($errors->all() as $message)
+     @foreach($errors->get('self_introduction') as $message)
      <p>{{ $message }}</p>
      @endforeach
     </div>
