e0adcb149ce58b47f873898065f82e7f2766308c
masal9pse <returnymgstokh@icloud.com>
2020-06-24 19:40:51 +0900

    バリデーションの日本語設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,8 +47,8 @@
      <ul class="nav navbar-nav navbar-right">
       <!-- Authentication Links -->
       @guest
-      <li><a href="{{ route('login') }}">Login</a></li>
-      <li><a href="{{ route('register') }}">Register</a></li>
+      <li><a href="{{ route('login') }}">ログインする</a></li>
+      <li><a href="{{ route('register') }}">登録する</a></li>
       @else
       <li class="dropdown">
        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
@@ -60,7 +60,7 @@
         <li>
          <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-          Logout
+          ログアウトする
          </a>
 
          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -79,7 +79,7 @@
    @yield('content')
   </main>
   <footer class='footer p20'>
-   <small class='copyright'>Laravel Song Text 2019 copyright</small>
+   <small class='copyright'>Laravel Song Text 2020 copyright</small>
   </footer>
  </div>
 
--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -54,7 +54,7 @@
      <ul class="nav navbar-nav navbar-right">
       <!-- Authentication Links -->
       @guest
-      <li><a href="{{ route('admin.login') }}">Login</a></li>
+      <li><a href="{{ route('admin.login') }}">ログインする</a></li>
       @else
       <li class="dropdown">
        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
@@ -66,7 +66,7 @@
         <li>
          <a href="{{ route('admin.logout') }}" onclick="event.preventDefault();
           document.getElementById('logout-form').submit();">
-          Logout
+          ログアウトする
          </a>
 
          <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
