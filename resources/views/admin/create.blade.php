df23f4e16e05f68a94221d76c298436bdf87bc8d
masal9pse <returnymgstokh@icloud.com>
2020-06-24 23:43:10 +0900

    スタイル修正その３

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -21,31 +21,32 @@
     </p>
    </div>
 
-   <div class="panel panel-primary">
-    <div class="panel-heading">
-     <h5 class="panel-title">
-      歌詞登録
-     </h5>
-    </div>
-    <div class="penel-body">
-     <div class="form-group row">
-      <form action="{{ route('admin.store')}}" method="post" enctype="multipart/form-data">
-       {{ csrf_field() }}
-       <label for="inputTitle" class="col-sm-2 col-form-label">タイトル</label>
-       <div class="col-sm-10">
-        @if($errors->has('title'))
-        @foreach($errors->get('title') as $message)
-        <div class="text-danger">
-         {{ $message }}
-        </div>
-        @endforeach
-        @endif
-        <input type="text" name="title">
-       </div>
-     </div>
+   <form action="{{ route('admin.store')}}" method="post" enctype="multipart/form-data">
+    {{ csrf_field() }}
 
-     <div class="form-group row">
-      <label for="inputDescription" class="col-sm-2 col-form-label">歌詞</label>
+    <div class="panel panel-primary">
+     <div class="panel-heading">
+      <h5 class="panel-title">
+       歌詞登録
+      </h5>
+     </div>
+     <br>
+     <div class="penel-body">
+      <label for="inputTitle" class="col-sm-2 col-form-label">タイトル</label>
+      <div class="col-sm-10">
+       @if($errors->has('title'))
+       @foreach($errors->get('title') as $message)
+       <div class="text-danger">
+        {{ $message }}
+       </div>
+       @endforeach
+       @endif
+       <input type="text" name="title" class="form-control" placeholder="タイトル">
+      </div>
+      <br>
+      <label for="inputDescription" class="col-sm-2 col-form-label">
+       歌詞
+      </label>
       <div class="col-sm-10">
        @if($errors->has('detail'))
        @foreach($errors->get('detail') as $message)
@@ -54,15 +55,12 @@
        </div>
        @endforeach
        @endif
-       <textarea name="detail" class="mt-5"></textarea>
+       <textarea name="detail" class="form-control" placeholder="歌詞を入力してください"></textarea>
       </div>
-     </div>
-
-     <div class="form-group row">
+      <br>
       <input type="file" class="form-control" name="file_name">
-     </div>
-
-     <div class="form-group row">
+      <br>
+      {{-- <div class="form-group row"> --}}
       <label for="inputTag" class="col-sm-2 col-form-label">タグをつける</label>
       <div class="col-sm-10">
        <div class="form-check form-check-inline">
@@ -76,11 +74,12 @@
         @endforeach
        </div>
       </div>
+      <br>
+      <input type="submit" value="登録する" class="btn btn-info">
      </div>
-     <input type="submit" value="登録する" class="btn btn-info">
     </div>
-   </div>
    </form>
+
    <br>
    <br>
    <div>
