fc8a3d51a1e230d1c0b2eadeab18d553a52b637b
masal9pse <returnymgstokh@icloud.com>
2020-06-29 09:43:26 +0900

    認証エラーの解消

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,9 +37,4 @@ class LoginController extends Controller
  {
   $this->middleware('guest')->except('logout');
  }
-
- protected function authenticated(Request $request, $user)
- {
-  return $user;
- }
 }
