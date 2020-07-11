cea1e2c45fd478ca2655db6499350266ced13bc7
masal9pse <returnymgstokh@icloud.com>
2020-07-11 16:58:41 +0900

    匿名コメント欄テスト終了

--- tests/Feature/ProblemTest.php
+++ tests/Feature/ProblemTest.php
@@ -15,17 +15,31 @@ class ProblemTest extends TestCase
   *
   * @return void
   */
- public function test_store()
+ public function test_問題点を１件登録する()
  {
-  //第二引数にPOST値の配列を渡すだけ
-  // error原因はsongsテーブルを参照しちゃってる
   $response = $this->post('/problems', [
    'name' => 'テストユーザー',
    'problem' => '歌大好き'
   ]);
 
-  //登録処理が完了して、一覧画面にリダイレクトすることを検証
   $response->assertRedirect('/');
-  // $response->assertRedirect()->route('songs.index');
+ }
+
+ public function test_問題点を複数登録する()
+ {
+  $response = $this->post('/problems', [
+   'problems' => [
+    [
+     'name' => 'テストユーザー1',
+     'problem' => '歌大好き'
+    ],
+    [
+     'name' => 'テストユーザー2',
+     'problem' => '歌大嫌い'
+    ],
+   ]
+  ]);
+
+  $response->assertRedirect('/');
  }
 }
