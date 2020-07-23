637ff0c870c502a3550cc2b125715bc2329f2302
masal9pse <returnymgstokh@icloud.com>
2020-07-23 10:57:32 +0900

    認証画面のUIを日本語化

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,14 +5,14 @@
  <div class="row">
   <div class="col-md-8 col-md-offset-2">
    <div class="panel panel-default">
-    <div class="panel-heading">Login</div>
+    <div class="panel-heading">管理ユーザーログイン</div>
 
     <div class="panel-body">
      <form class="form-horizontal" method="POST" action="{{ route('admin.login') }}">
       {{ csrf_field() }}
 
       <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-       <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+       <label for="email" class="col-md-4 control-label">メールアドレス</label>
 
        <div class="col-md-6">
         <input id="email" type="email" class="form-control" name="email" value="admin@admin.com" required autofocus>
@@ -26,7 +26,7 @@
       </div>
 
       <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-       <label for="password" class="col-md-4 control-label">Password</label>
+       <label for="password" class="col-md-4 control-label">パスワード</label>
 
        <div class="col-md-6">
         <input id="password" type="password" class="form-control" name="password" value="password123" required>
@@ -39,29 +39,28 @@
        </div>
       </div>
 
-      <div class="form-group">
+      {{-- <div class="form-group">
        <div class="col-md-6 col-md-offset-4">
         <div class="checkbox">
          <label>
           <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-         </label>
-        </div>
-       </div>
-      </div>
-
-      <div class="form-group">
-       <div class="col-md-8 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
-         Login
-        </button>
-        Forgot Your Password?
-        </a>
-       </div>
-      </div>
-     </form>
+      </label>
     </div>
    </div>
+  </div> --}}
+
+  <div class="form-group">
+   <div class="col-md-8 col-md-offset-4">
+    <button type="submit" class="btn btn-primary">
+     ログイン
+    </button>
+    </a>
+   </div>
   </div>
+  </form>
  </div>
+</div>
+</div>
+</div>
 </div>
 @endsection
