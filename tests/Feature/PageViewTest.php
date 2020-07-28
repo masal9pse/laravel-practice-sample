4acc05864637c4f49669d233bca0daa3197e95a8
masal9pse <returnymgstokh@icloud.com>
2020-07-28 16:02:40 +0900

    管理画面の表示をテスト

---
+++ tests/Feature/PageViewTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Admin;
+
+class TopPageTest extends TestCase
+{
+ use RefreshDatabase;
+ /**
+  * A basic test example.
+  *
+  * @return void
+  */
+ public function test_topPageが正常に表示される()
+ {
+  $response = $this->get('/');
+
+  $response->assertStatus(200);
+ }
+
+ public function test_管理認証画面が正常に表示される()
+ {
+  $response = $this->get('/admin/login');
+
+  $response->assertStatus(200);
+ }
+
+ public function test_管理画面トップが正常に表示される()
+ {
+  $this->withoutExceptionHandling();
+  $this->admin = factory(Admin::class)->create();
+  $response = $this->actingAs($this->admin, 'admin');
+  $response = $this->get('/admin/create');
+
+  $response->assertStatus(200);
+ }
+}
