7b33815647c2ecebeaa90970341a3d5b90b91cb7
masal9pse <returnymgstokh@icloud.com>
2020-08-10 04:06:32 +0900

    change color topPageAdminButton

--- resources/views/components/top_search.blade.php
+++ resources/views/components/top_search.blade.php
@@ -1,5 +1,5 @@
 <form method="GET" action="{{ route('songs.index') }}" class="form-inline my-2 my-lg-0">
  <input class="form-control mr-sm-2" name="search" type="search" placeholder="タイトルを入力" aria-label="Search">
  <button class="btn btn-success my-2 my-sm-0" type="submit">検索する</button>
- <a href="{{ route('admin.create')}}" class="btn btn-primary">管理画面へ移動(通常は存在しません)</a>
+ <a href="{{ route('admin.create')}}" class="btn btn-danger">管理画面へ移動(通常は存在しません)</a>
 </form>
