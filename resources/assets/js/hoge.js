4e23063aa0a989d30575094c34d350367a09bca9
masal9pse <returnymgstokh@icloud.com>
2020-10-26 05:24:00 +0900

    pureJSで作成したapiを呼び出した。

--- resources/assets/js/hoge.js
+++ resources/assets/js/hoge.js
@@ -1,1 +1,11 @@
 console.log('hello masato');
+
+axios.get('api/user')
+ .then(function(response) {
+  console.log(response);
+ })
+
+axios.get('api/user/55')
+ .then(function(response) {
+  console.log(response);
+ })
